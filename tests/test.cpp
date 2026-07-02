1dce613670671dfd612b2050dd5c29b334468027
hungg261 <phanthanhhung.26012010@gmail.com>
2026-07-02 09:06:15 +0700

    update test file

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,4 @@
-#include "randlib.h"
+#include "../randlib.h"
 #include<bits/stdc++.h>
 // #define int long long
 using namespace std;
@@ -9,7 +9,23 @@ signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     NumberGen NumGen;
-    cout << NumGen.Rand(1, 10) << '\n';
+    GraphGen Ggen;
+
+    int n = 100, q = 100;
+    cout << n << ' ' << q << '\n';
+
+    for(int i = 1; i <= n; ++i){
+        cout << NumGen.Rand(1, 2) << ' ';
+    }
+    cout << '\n';
+
+    for(const pair<int, int>& p: Ggen.randomTree(n)){
+        cout << p.first << ' ' << p.second << '\n';
+    }
+    for(int i = 1; i <= q; ++i){
+        int u = NumGen.Rand(1, n), v = NumGen.Rand(1, n);
+        cout << u << ' ' << v << '\n';
+    }
 
     return 0;
 }
